033347cf6b65bde7b0889e15d2cb7ecf9dc537b1
rad304 <y.radouani929@gmail.com>
2020-10-25 20:36:56 +0100

    initUsers implementation

--- src/main/java/com/rad/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/rad/ecommerceapi/EcommerceApiApplication.java
@@ -1,13 +1,22 @@
 package com.rad.ecommerceapi;
 
+import com.rad.ecommerceapi.services.DBInitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApiApplication {
+public class EcommerceApiApplication implements CommandLineRunner {
+
+    @Autowired private DBInitService dbInitService;
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApiApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        dbInitService.initUsers();
+    }
 }
--- src/main/java/com/rad/ecommerceapi/services/DBInitServiceImpl.java
+++ src/main/java/com/rad/ecommerceapi/services/DBInitServiceImpl.java
@@ -1,15 +1,40 @@
 package com.rad.ecommerceapi.services;
 
+import com.rad.ecommerceapi.dao.UserRepository;
+import com.rad.ecommerceapi.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Random;
 
 @Service("DBInitService")
 @Transactional
 public class DBInitServiceImpl implements DBInitService {
+    @Autowired private UserRepository userRepository;
     @Override
     public void initUsers() {
-
+        for(int i = 0; i < 5; i++){
+            User user = new User();
+            user.setUsername("user"+(i+1));
+            user.setFirstname("user"+(i+1)+"F");
+            user.setLastname("user"+(i+1)+"L");
+            if (new Random().nextInt(1) < 0.5) {
+                user.setGender("male");
+            } else {
+                user.setGender("female");
+            }
+            user.setEmail("user"+(i+1)+"@example.com");
+            user.setAddress("user"+(i+1)+" address");
+            user.setPassword("123");
+            if (new Random().nextInt(1) < 0.5) {
+                user.setRole("user");
+            } else {
+                user.setRole("admin");
+            }
+            user.setTel("0000000000");
+            userRepository.save(user);
+        }
     }
 
     @Override
